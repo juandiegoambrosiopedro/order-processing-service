eec540e1d4185fc2723f85d074bed7dfccbc15e8
JAAMBROS <juandiegoambrosiopedro@gmail.com>
2025-12-16 10:31:16 -0600

    feat: se agrega enum Canal para canales de procesamiento (WEB, APP, CALLCENTER)

---
+++ src/main/java/mx/com/leyva/order/enums/Canal.java
@@ -0,0 +1,10 @@
+package mx.com.leyva.order.enums;
+
+/**
+ * Enum que representa los distintos canales a través de los cuales se pueden procesar las órdenes.
+ */
+public enum Canal {
+    WEB,
+    APP,
+    CALLCENTER
+}
